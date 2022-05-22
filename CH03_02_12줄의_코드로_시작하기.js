bf0628ed3bf08913e13b67ddf02e349c3816b92b
nrbns357 <gusgh357@gmail.com>
2022-05-22 21:50:31 +0900

    ADD: 코드 래팩토링

--- CH03_02_12줄의_코드로_시작하기.js
+++ CH03_02_12줄의_코드로_시작하기.js
@@ -3,38 +3,49 @@ let ajax = new XMLHttpRequest();
 const NEW_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https:api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEW_URL, false);
-ajax.send();
+const getData = (URL) => {
+  ajax.open("GET", URL, false);
+  ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+};
+
+const newsFeed = getData(NEW_URL);
 
 const ul = document.createElement("ul");
-const container = document.createElement("div");
+const content = document.createElement("div");
 
 window.addEventListener("hashchange", () => {
   // hash는 #, 좌표이다.
   const id = location.hash.substring(1);
 
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
   const title = document.createElement("h1");
 
   title.innerHTML = newsContent.title;
 
-  container.appendChild(title);
+  content.appendChild(title);
 });
 
 for (let i = 0; i < newsFeed.length; i++) {
+  const div = document.createElement("div");
   const li = document.createElement("li");
   const a = document.createElement("a");
 
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-  li.appendChild(a);
-  ul.appendChild(li);
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `; //innerHTML은 문자열 안에 태그(마크업 구조)가 들어있다면 태그를 DOM으로 인식을 하여 읽는다.
+
+  // a.href = `#${newsFeed[i].id}`;
+  // a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  // li.appendChild(a);
+  // console.log(div.children[i]); //배열의 형식
+  ul.appendChild(div.firstElementChild);
 }
 
 Reroot.appendChild(ul);
-Reroot.appendChild(container);
+Reroot.appendChild(content);
--- dist/CH03_02_12줄의_코드로_시작하기.31a182d4.js
+++ dist/CH03_02_12줄의_코드로_시작하기.31a182d4.js
@@ -122,33 +122,40 @@ var Reroot = document.getElementById("root");
 var ajax = new XMLHttpRequest();
 var NEW_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https:api.hnpwa.com/v0/item/@id.json";
-ajax.open("GET", NEW_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
+
+var getData = function getData(URL) {
+  ajax.open("GET", URL, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+};
+
+var newsFeed = getData(NEW_URL);
 var ul = document.createElement("ul");
-var container = document.createElement("div");
+var content = document.createElement("div");
 window.addEventListener("hashchange", function () {
   // hash는 #, 좌표이다.
   var id = location.hash.substring(1);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
+  var newsContent = getData(CONTENT_URL.replace("@id", id));
   var title = document.createElement("h1");
   title.innerHTML = newsContent.title;
-  container.appendChild(title);
+  content.appendChild(title);
 });
 
 for (var i = 0; i < newsFeed.length; i++) {
+  var div = document.createElement("div");
   var li = document.createElement("li");
   var a = document.createElement("a");
-  a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
-  li.appendChild(a);
-  ul.appendChild(li);
+  div.innerHTML = "\n    <li>\n      <a href=\"#".concat(newsFeed[i].id, "\">\n      ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n      </a>\n    </li>\n  "); //innerHTML은 문자열 안에 태그(마크업 구조)가 들어있다면 태그를 DOM으로 인식을 하여 읽는다.
+  // a.href = `#${newsFeed[i].id}`;
+  // a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  // li.appendChild(a);
+  // console.log(div.children[i]); //배열의 형식
+
+  ul.appendChild(div.firstElementChild);
 }
 
 Reroot.appendChild(ul);
-Reroot.appendChild(container);
+Reroot.appendChild(content);
 },{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -177,7 +184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54092" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56341" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
